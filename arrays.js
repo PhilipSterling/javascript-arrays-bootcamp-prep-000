01ac86140846382300cd07418431a96671efef9f
Philip Sterling <philipssterlings@gmail.com>
2019-05-23 07:30:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -17,3 +17,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
   array = array.push(element)
   return array
 }
+function accessElementInArray(array, index) {
+  return array[index]
+}
