aba7a45241bc3e4a9e3ef3eef29ba6a31067e6b1
Philip Sterling <philipssterlings@gmail.com>
2019-05-23 07:25:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,11 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array = [element, ...array]
   return array
 }
+function addElementToEndOfArray(array, element) {
+  var temp = [element, ...array]
+  return temp
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+  array = [element, ...array]
+  return array
+}
